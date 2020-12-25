965703b442063c35514f23c4705db3038b35adbf
hamiguazzz <429556240@qq.com>
2020-12-25 15:24:27 +0800

    框架

---
+++ Rabi_description.py
@@ -0,0 +1,24 @@
+from qutip import *
+N = 50
+# other ops
+
+def Get_H_operator(wc,wa,wr,isRWA):#哈密顿量
+    pass
+
+def Get_initial_state(n,ce,cg):#初态
+    pass
+
+def Get_c_ops():#衰减
+    pass
+
+def Get_omega(x,y):#三角拟合
+    pass
+
+def Get_N_op_photon():#aa+
+    pass
+
+def Get_N_op_ground():#s+s-
+    pass
+
+def Get_N_op_excited():#s-s+
+    pass
---
+++ Rabi_example.py
@@ -0,0 +1,17 @@
+from Rabi_description import *
+
+
+def example1():  # 破坏wa=wc
+    pass
+
+
+def example2():  # 无衰减振荡，画eg能量图，画ec能量图，验证wr
+    pass
+
+
+def example3():  # RWA影响
+    pass
+
+
+def example4():  # 衰减的影响
+    pass
